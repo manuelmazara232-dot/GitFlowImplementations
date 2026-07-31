af76bbb8aa756bf1d26674f6eb4d8aa8c44b6d94
Manuel <manuelmazara232@gmail.com>
2026-07-31 16:09:29 -0400

    DBcontext implementation and minor changes in models.

---
+++ GitFlow.Entities/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+
+namespace Gitflow.entites.Models;
+
+public partial class Person
+{
+    public int Id { get; set; }
+
+    public string Firstname { get; set; }
+
+    public string Lastname { get; set; }
+
+    public DateOnly? Birthdate { get; set; }
+
+    public string Dni { get; set; }
+
+    public string Gender { get; set; }
+}
