8d44f893f14abea3ca8b637f39d336dad0f35149
Manuel <manuelmazara232@gmail.com>
2026-07-31 15:31:37 -0400

    Adjustemnts in GitFlow.DataAcces and creation of People entity in GitFlowEntities.

---
+++ GitFlow.Entities/People.cs
@@ -0,0 +1,12 @@
+﻿namespace GitFlow.Entities
+{
+    internal class People
+    {
+        public int ID { get; set; }
+        public string? FIRSTNAME { get; set; }
+        public string? LASTNAME {  get; set; }
+        public DateOnly? BIRTHDATE { get; set; }   
+        public string? DNI {  get; set; }
+        public string? GENDER { get; set; }
+    }
+}
