91de7eaaea2eb1d0b88a2f7e11f6b3059c2af0bd
Manuel <manuelmazara232@gmail.com>
2026-07-31 14:37:54 -0400

    Add project files.

---
+++ Gitflow.DataAcces/ConFiles/DBsetup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace Gitflow.DataAcces.ConFiles
+{
+    internal class DBsetup
+    {
+        string serverConn = ConfigurationManager.ConnectionStrings["ServerConnString"]?.ConnectionString;
+        string appConn = ConfigurationManager.ConnectionStrings["appConn"]?.ConnectionString;
+        
+
+        private void DBExists(string ServerConnection, string DatabaseName)
+        {
+            using(new SqlConnection(ServerConnection))
+            {
+
+            }
+        }
+    }
+}
